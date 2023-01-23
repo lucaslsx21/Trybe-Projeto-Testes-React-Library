e64c45fbda000c8059a1c7ebd9cd56a86384b7e7
lucaslsx21 <lucaslsilva@live.com>
2023-01-23 11:54:09 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Teste o componente NotFound.js', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    const { getByRole } = render(<NotFound />);
+
+    const pageFound = getByRole('heading', {
+      name: /Page requested not found/i });
+    expect(pageFound).toBeDefined();
+  });
+
+  it('Teste se a página mostra a imagem src', () => {
+    const { getByRole } = render(<NotFound />);
+
+    const showImg = getByRole('img', {
+      name: /Pikachu crying because the page requested was not found/i });
+    expect(showImg).toBeDefined();
+    expect(showImg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
